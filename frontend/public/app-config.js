a4487d05d6bf2503f43759a0a6a171c34991f72b
damzcore <damzcore@gmail.com>
2026-05-31 12:20:08 +0700

    feat: use smart dynamic port resolution in app-config.js for LAN (5000) and WAN (5050)

--- frontend/public/app-config.js
+++ frontend/public/app-config.js
@@ -1,4 +1,7 @@
 window.APP_CONFIG = {
   // Client bisa mengganti URL & Port ini secara manual di dalam folder dist/app-config.js
-  API_URL: "http://localhost:5000"
+  API_URL: window.location.protocol + "//" + window.location.hostname + ":" + 
+    ((window.location.hostname === 'localhost' || 
+      window.location.hostname === '127.0.0.1' || 
+      window.location.hostname === '192.168.13.190') ? '5000' : '5050')
 };
